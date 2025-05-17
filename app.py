70177e173055a944f12a6dc8d0461b09e64b031f
carmelon2010 <carmelon2010@gmail.com>
2025-05-17 20:21:52 +0300

    fixing prefix

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from Flask import Flask
 from views import views
 
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix='/views')
+app.register_blueprint(views, url_prefix='/')
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+app.run(debug=True)

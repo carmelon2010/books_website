1bd1fd1be9071919b8b7bc274dbd49b6d7d0c610
carmelon2010 <carmelon2010@gmail.com>
2025-05-18 09:19:55 +0300

    fixed paths again and again and again and again and again

--- docs/views.py
+++ docs/views.py
@@ -15,7 +15,7 @@ def home():
         add_book([user_input,find_book_link(user_input)])
         load_books()
         return redirect(url_for('views.home'))  # Prevent resubmission
-    return render_template('Templates/index.html', name=names)
+    return render_template('index.html', name=names)
 
 @views.route('/profile', methods=['GET', 'POST'])
 def profile():
@@ -28,7 +28,7 @@ def profile():
             print(f"Received input: {user_input}")  # DEBUG
             delete_book(names[int(user_input)][0])
             names.pop(int(user_input))
-        return render_template('Templates/developer.html', name=names)
+        return render_template('developer.html', name=names)
 
 @views.route('/developer-mode')
 def developer_mode():

71778a74a00d246a7ace9cc46abd8cd8ce46c327
carmelon2010 <carmelon2010@gmail.com>
2025-05-19 16:47:27 +0300

    revert changes

--- views.py
+++ views.py
@@ -10,7 +10,7 @@ def home():
     if request.method == 'POST':
         user_input = request.form['user_input']
         print(f"Received input: {user_input}")  # DEBUG
-        names.append([user_input,find_book_link(user_input), 'all'])
+        names.append([user_input,find_book_link(user_input)])
         add_book([user_input,find_book_link(user_input)])
         load_books()
         return redirect(url_for('views.home'))  # Prevent resubmission
@@ -18,25 +18,17 @@ def home():
 
 @views.route('/profile', methods=['GET', 'POST'])
 def profile():
-    global names
     args = request.args
     name = args.get('name')
-    if name == 'developer':
+    if name == 'carmel':
+        global names
         if request.method == 'POST':
             user_input = request.form.get('delete')
             print(f"Received input: {user_input}")  # DEBUG
             delete_book(names[int(user_input)][0])
             names.pop(int(user_input))
         return render_template('developer.html', name=names)
-    else:
-        if request.method == 'POST':
-            user_input = request.form['user_input']
-            print(f"Received input: {user_input}")  # DEBUG
-            names.append([user_input, find_book_link(user_input), name])
-            add_book([user_input, find_book_link(user_input)])
-            load_books()
-        return render_template('index.html', name=[sublist for sublist in names if sublist and sublist[-1] == name])
 
 @views.route('/developer-mode')
 def developer_mode():
-    return redirect(url_for('views.profile', name='developer'))
+    return redirect(url_for('views.profile', name='carmel'))

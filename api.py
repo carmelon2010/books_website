4ba248efa1b651805a9340434727fd53aa93788e
carmelon2010 <carmelon2010@gmail.com>
2025-05-19 17:09:50 +0300

    fix linking

--- api.py
+++ api.py
@@ -2,6 +2,6 @@ from googlesearch import search
 
 def find_book_link(book_name):
     query = f"{book_name} read online"
-    results = search(query, num_results=1)
+    results = search(query, num_results=2)
     for i in results:
         return i
